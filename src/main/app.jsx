71cd11894bfa5c491e9825051a43d673f40dcd06
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2019-12-13 11:24:27 -0300

    Adding todo + about

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,7 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+
 import React from "react";
+import Todo from "../todo/todo";
+import About from "../about/about";
 
 export default props => (
   <div className="container">
-    <h1>Teste</h1>
+    <Todo />
+    <About />
   </div>
 );
